a9b58a71f21afc0b9489c5978e4857f7b14c64ac
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-06-25 16:56:08 -0300

    Melhor adequação para exibir/esconder campos de form do tema de museus

--- assets/js/modal-museu.js
+++ assets/js/modal-museu.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
     var modal_el = 'select[name="mus_EnCorrespondencia_mesmo"]';
     if ($(modal_el).length > 0) {
         var _toggable = '.entity-modal .en-correspondencia';
-        $(_toggable).hide();
+        $(_toggable).hide().removeAttr('required').css('margin-left', 20);
         $(modal_el).change( function() {
+            var _answer = $(this).val();
             $(_toggable).toggle();
+            toggleEnderecoCorrespondencia(_answer, _toggable);
         });
+    }
+
+    function toggleEnderecoCorrespondencia(mesmo_endereco, $element) {
+        if (mesmo_endereco === 'não') {
+            $($element + ' input').attr('required','required');
+        } else if (mesmo_endereco === 'sim') {
+            $($element + ' input').removeAttr('required');
+        }
     }
 });
