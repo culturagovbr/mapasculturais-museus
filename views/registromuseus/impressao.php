01ac8f31061cf5fb886666204b14f65788a05ac1
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-10-17 18:29:37 -0300

    Diminui espaçamento para data/assinaturas ao final fiquem na mesma página de impressão (ref #92)

--- views/registromuseus/impressao.php
+++ views/registromuseus/impressao.php
@@ -31,7 +31,7 @@
 
         <div style="width:100%;text-align:center;"><img src="<?php $this->asset('img/registromuseus.gif')?>" style="width: 250px;text-align:center;margin-bottom: 50px;"></div>
 
-        <div style="line-height: 2;">
+        <div style="line-height: 1.7;">
             Eu, ______________________________________________, portador do RG _________________, expedido em ______________, pelo órgão _________________, inscrito no CPF sob o nº ________________, residente e domiciliado em ________________________________________________________, no município de _________________________________, estado de _______, declaro para fins da Política Nacional de Museus que sou responsável legal pelo Museu ______________________________________________________________, situado à ___________________________________________________________, no município de _______________________________________________, no estado de _________, e que tenho ciência do Estatuto de Museus, instituído pela Lei nº 11.904, de 14 de janeiro de 2009, e de seu respectivo Decreto regulamentador, nº 8.124, de 17 de outrubro de 2013, e das demais normas federais referentes à Legislação Museológica.
             Por ser verdade as informações prestadas, solicito o Registro do referido Museu e comprometo-me a manter as informações desta instiuição requerente, atualizadas junto à entidade registradora de origem.
 
